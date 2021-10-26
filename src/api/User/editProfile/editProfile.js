de67b50f04a33615fbbd3ad1b9881b52ca9cef39
Bort <sentient.corn.golem@gmail.com>
2021-10-26 15:30:33 +1300

    [Issue #10] Whitelist characters for handle

--- src/api/User/editProfile/editProfile.js
+++ src/api/User/editProfile/editProfile.js
@@ -1,9 +1,16 @@
+const isHandleValid = ({ handle }) => {
+  const hasSpecialCharacters = /[^a-z0-9_-]/gi.test(handle);
+  return !hasSpecialCharacters;
+}
+
 module.exports = {
   Mutation: {
     editProfile: async (parent, args, ctx) => {
       // 1. make sure the user is authenticated
       const userId = ctx.getUserId(ctx);
       if (!userId) throw Error("You need to be authenticated.");
+
+      if (!isHandleValid(args)) throw Error("Your handle must only contain alphanumeric characters, underscores or dashes.");
 
       // 2.make sure handle isn't taken
       const exists = await ctx.prisma.user.findFirst({
