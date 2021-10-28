28947f08f17cfbb603dd263152ca644803f77091
Harkl <3zy3@protonmail.com>
2021-10-28 16:58:13 +1300

    add sentry to backend

--- src/server.js
+++ src/server.js
@@ -4,6 +4,13 @@ const schema = require("./schema");
 const { ApolloServer } = require("apollo-server");
 const { PrismaClient } = require("@prisma/client");
 const { getUserId } = require("./utils");
+const Sentry = require("@sentry/node");
+const Tracing = require("@sentry/tracing");
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  tracesSampleRate: 1.0,
+});
 
 const prisma = new PrismaClient();
 
