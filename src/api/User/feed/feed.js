d020de3f3937edfe0a7d0b810035c63a79412fc2
Harkl <3zy3@protonmail.com>
2021-11-19 09:35:10 +1300

    use global indicator on fgeed query

--- src/api/User/feed/feed.js
+++ src/api/User/feed/feed.js
@@ -1,52 +1,71 @@
 module.exports = {
   Query: {
-    feed: async (parent, args, ctx) => {
-      const {offset, limit} = args;
+    feed: async (parent, _args, ctx) => {
+      const { offset, limit } = _args;
       // 1. make sure the user is authenticated
-      const userId = ctx.getUserId(ctx);
-      if (!userId) throw Error("You need to be authenticated");
-      if (typeof offset !== 'number') throw Error("Offset argument for limit is missing");
-      if (typeof limit !== 'number') throw Error("Limit argument for offset is missing");
+      const authUser = _args.global ? false : true;
+      const userId = ctx.getUserId(ctx, authUser);
 
-      // get the tweets of the user and the people whom they are following
-      const following = await ctx.prisma.user
-        .findFirst({ where: { id: userId } })
-        .following();
+      if (typeof offset !== "number")
+        throw Error("Offset argument for limit is missing");
+      if (typeof limit !== "number")
+        throw Error("Limit argument for offset is missing");
 
-      const retweets = await ctx.prisma.retweet.findMany({
-        where: { user: { id: { in: following.map((user) => user.id) } } },
-      });
+      let tweets = [];
+      if (userId) {
+        // get the tweets of the user and the people whom they are following
+        const following = await ctx.prisma.user
+          .findFirst({ where: { id: userId } })
+          .following();
 
-      const tweets = await ctx.prisma.tweet.findMany({
-        where: {
-          OR: [
-            {
-              // shows tweets by people user follows
-              user: {
-                id: {
-                  in: following.map((user) => user.id).concat([userId]),
+        const retweets = await ctx.prisma.retweet.findMany({
+          where: { user: { id: { in: following.map((user) => user.id) } } },
+        });
+
+        tweets = await ctx.prisma.tweet.findMany({
+          where: {
+            OR: [
+              {
+                // shows tweets by people user follows
+                user: {
+                  id: {
+                    in: following.map((user) => user.id).concat([userId]),
+                  },
                 },
               },
-            },
-            {
-              // shows tweets retweeted by people user follows
-              id: { 
-                in: retweets.map((rt) => rt.tweetId),
+              {
+                // shows tweets retweeted by people user follows
+                id: {
+                  in: retweets.map((rt) => rt.tweetId),
+                },
               },
-            },
-          ],
-        },
-        orderBy: {
-          createdAt: "desc",
-        },
-        include: {
-          user: true,
-          files: true,
-          reactions: true,
-        },
-        skip: offset,
-        take: limit,
-      });
+            ],
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+          include: {
+            user: true,
+            files: true,
+            reactions: true,
+          },
+          skip: offset,
+          take: limit,
+        });
+      } else {
+        tweets = await ctx.prisma.tweet.findMany({
+          orderBy: {
+            createdAt: "desc",
+          },
+          include: {
+            user: true,
+            files: true,
+            reactions: true,
+          },
+          skip: offset,
+          take: limit,
+        });
+      }
 
       return tweets;
     },
